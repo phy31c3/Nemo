59a5e63e4e4e59a21dddd36f56d7acb068f2f53e
JongsunYoo <phy31c3@gmail.com>
2020-03-13 03:54:03 +0900

    #1: NemoRecyclerView 추가

---
+++ nemo/src/main/java/kr/co/plasticcity/nemo/NemoRecyclerView.kt
@@ -0,0 +1,10 @@
+package kr.co.plasticcity.nemo
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+class NemoRecyclerView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : RecyclerView(context, attrs, defStyleAttr)
+{
+
+}
