f43fbdbbb98a83b3ac9f1758cb1b0e8e89e33b62
JongsunYoo <phy31c3@gmail.com>
2020-03-20 01:17:55 +0900

    #1: 인터페이스 정의 중(2)

--- app/src/main/java/kr/co/plasticcity/nemo/demo/MainActivity.kt
+++ app/src/main/java/kr/co/plasticcity/nemo/demo/MainActivity.kt
@@ -2,7 +2,9 @@ package kr.co.plasticcity.nemo.demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.plasticcity.nemo.NemoRecyclerView
 import kr.co.plasticcity.nemo.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
@@ -12,9 +14,45 @@ class MainActivity : AppCompatActivity()
 		super.onCreate(savedInstanceState)
 		ActivityMainBinding.inflate(layoutInflater).also { binding ->
 			setContentView(binding.root)
-			recyclerView {
-			
+			val header = NemoRecyclerView.model("null") { this }
+			val accounts = NemoRecyclerView.model<String>(listOf("", "")) { hashCode() }
+			val groups = recyclerView(orientation = RecyclerView.VERTICAL, reverseLayout = false) {
+				useSnap = false
+				group(model = header, view = ActivityMainBinding::class, tag = "header") {
+					allowDragAndDrop = false
+					allowSwipeToDismiss = false
+					bind { data, binding ->
+						println(groupPos)
+						println(globalPos)
+					}
+					placeHolder(num = 2) { binding ->
+						println(groupPos)
+						println(globalPos)
+					}
+					divider {
+						sizeDp = 3
+						color = "#ff00ff00"
+						colorRes = R.color.colorPrimary
+						drawableRes = R.drawable.ic_launcher_background
+						show = { BEGINNING and MIDDLE and END }
+					}
+				}
+				space {
+					sizeDp = 100
+					color = "#ff00ff00"
+					colorRes = R.color.colorPrimary
+					fillViewport = true
+					fillWeight = 1f
+				}
+				group(model = accounts, view = ActivityMainBinding::class) {
+				
+				}
 			}
+			groups.bringForward("header")
+			groups.bringToFront("header")
+			groups.sendBackward("header")
+			groups.sendToBack("header")
+			groups.swap("header", "list")
 		}
 	}
 }
--- nemo/src/main/java/kr/co/plasticcity/nemo/NemoRecyclerView.kt
+++ nemo/src/main/java/kr/co/plasticcity/nemo/NemoRecyclerView.kt
@@ -4,44 +4,104 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import kotlin.reflect.KClass
 
 class NemoRecyclerView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : RecyclerView(context, attrs, defStyleAttr)
 {
-	operator fun invoke(@RecyclerView.Orientation orientation: Int = VERTICAL, reverseLayout: Boolean = false, block: Define.() -> Unit): GroupOrder
+	@DslMarker
+	internal annotation class Marker
+	
+	companion object
+	{
+		fun <M> model(model: M, key: (M.() -> Any?)? = null): Model.Singleton<M?>
+		{
+			TODO()
+		}
+		
+		fun <M> model(list: List<M>, key: (M.() -> Any?)? = null): Model.List<M>
+		{
+			TODO()
+		}
+		
+		fun <M> model(list: MutableList<M>, key: (M.() -> Any?)? = null): Model.MutableList<M>
+		{
+			TODO()
+		}
+	}
+	
+	operator fun invoke(@RecyclerView.Orientation orientation: Int = VERTICAL, reverseLayout: Boolean = false, block: Define.() -> Unit): GroupArrange
 	{
 		TODO()
 	}
-	
-	@DslMarker
-	internal annotation class Marker
 	
 	@Marker
 	interface Define
 	{
 		var useSnap: Boolean
 		
-		fun <M, V : ViewBinding> group(model: Model<M>, view: V, tag: Any? = null, block: GroupDefine.() -> Unit)
+		fun <M, V : ViewBinding> group(model: Model<M>, view: KClass<V>, tag: Any? = null, block: GroupDefine<M, V>.() -> Unit)
 		fun space(block: SpaceDefine.() -> Unit)
 	}
 	
 	@Marker
-	interface GroupDefine
+	interface GroupDefine<M, V : ViewBinding>
 	{
+		var allowDragAndDrop: Boolean
+		var allowSwipeToDismiss: Boolean
+		
+		fun bind(block: Bind.(data: M, binding: V) -> Unit)
+		fun placeHolder(num: Int = 1, block: Bind.(binding: V) -> Unit)
+		fun divider(block: Divider.() -> Unit)
+	}
 	
+	@Marker
+	interface Bind
+	{
+		val groupPos: Int
+		val globalPos: Int
+	}
+	
+	@Marker
+	interface Divider
+	{
+		var sizeDp: Int
+		var color: String
+		var colorRes: Int
+		var drawableRes: Int
+		var show: Position.() -> Int
+		
+		@Marker
+		object Position
+		{
+			const val BEGINNING = 0x7FFFFFFE
+			const val MIDDLE = 0x7FFFFFFD
+			const val END = 0x7FFFFFFB
+		}
 	}
 	
 	@Marker
 	interface SpaceDefine
 	{
+		var sizeDp: Int
+		var color: String
+		var colorRes: Int
+		var fillViewport: Boolean
+		var fillWeight: Float
+	}
 	
+	interface GroupArrange
+	{
+		fun bringForward(tag: Any)
+		fun bringToFront(tag: Any)
+		fun sendBackward(tag: Any)
+		fun sendToBack(tag: Any)
+		fun swap(tag1: Any, tag2: Any)
 	}
 	
 	interface Model<M>
 	{
 		interface Singleton<M> : Model<M>
-		interface List<M> : Model<M>
-		interface MutableList<M> : Model<M>
+		interface List<M> : Model<M>, kotlin.collections.List<M>
+		interface MutableList<M> : Model<M>, kotlin.collections.MutableList<M>
 	}
-	
-	interface GroupOrder
 }
