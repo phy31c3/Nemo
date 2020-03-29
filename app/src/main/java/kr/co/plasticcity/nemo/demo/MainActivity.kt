27f7378c91e836aba17c067c3cd0cff6d3b0a6d4
JongsunYoo <phy31c3@gmail.com>
2020-03-29 09:24:43 +0900

    #1: 데모 Activity 구현 중

--- app/src/main/java/kr/co/plasticcity/nemo/demo/MainActivity.kt
+++ app/src/main/java/kr/co/plasticcity/nemo/demo/MainActivity.kt
@@ -2,35 +2,62 @@ package kr.co.plasticcity.nemo.demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.plasticcity.nemo.widget.NemoRecyclerView
 import kr.co.plasticcity.nemo.demo.databinding.ActivityMainBinding
 import kr.co.plasticcity.nemo.demo.databinding.ItemHeaderBinding
+import kr.co.plasticcity.nemo.demo.databinding.ItemListBinding
+import kr.co.plasticcity.nemo.demo.databinding.ItemListHeaderBinding
 
 class MainActivity : AppCompatActivity()
 {
-	private val header = NemoRecyclerView.model<Header?>(Header("헤더의 제목", "헤더의 내용"))
+	val colors = listOf(
+			"#EF9A9A",
+			"#F5B7B1",
+			"#C39BD3",
+			"#7FB3D5",
+			"#F9E79F",
+			"#7DCEA0",
+			"#F8C471",
+			"#F0B27A",
+			"#EDBB99",
+			"#F7F9F9",
+			"#D5DBDB",
+			"#ABB2B9",
+			"#B2BABB",
+			"#85C1E9",
+			"#BB8FCE",
+			"#A9DFBF",
+			"#FAE5D3",
+			"#F1C40F",
+			"#4FC3F7",
+			"#AED581"
+	)
 	
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
+		val header = NemoRecyclerView.model(Header("NemoRecyclerView 데모", "쉽고 빠른 멀티 타입 리사이클러뷰 만들기!"))
+		val listHeader = NemoRecyclerView.model(ItemHeader("아이템 갯수: 0"))
+		val list = NemoRecyclerView.model(mutableListOf<Item>())
+		
 		super.onCreate(savedInstanceState)
 		ActivityMainBinding.inflate(layoutInflater).also { binding ->
 			setContentView(binding.root)
 			recyclerView {
 				group(header, ItemHeaderBinding::inflate) {
-					bind { data, binding ->
-						TODO()
-					}
+				
+				}
+				group(listHeader, ItemListHeaderBinding::inflate) {
+				
+				}
+				group(list, ItemListBinding::inflate) {
+				
 				}
 			}
 		}
 	}
-	
-	fun onDataReceived()
-	{
-		TODO()
-	}
 }
 
 private data class Header(val title: String, val content: String)
+private data class ItemHeader(val text: String)
+private data class Item(val text: String)
